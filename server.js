8374289f16626338f09656c331a6aa8f7af9bc66
sanuhembrom <sanuhembrom3@gmail.com>
2025-03-21 10:14:28 +0530

    devServ

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const port = 8081;
+
+http
+   .createServer((req, res) => { // call back function
+    res.writeHead(200, { "content-Type": "text/html" });
+    res.write("<h2>hey server started :-) 123456 </h2>");
+    res.end();
+   })
+   .listen(port, () => { // call back function
+    console.log(`NodeJs Server Started Running on port ${port}`);
+   });
+
+   // http://localhost:8081
+
+   
